26faf621542bc64bfee337ca0a148ebfdd080a21
Aman <ak193566@gmail.com>
2023-12-06 21:34:48 +0530

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,30 +2,46 @@ import React from 'react'
 import 'remixicon/fonts/remixicon.css'
 
 function Header() {
+  const menuItemsName = [
+    {name:"Air Force",},
+    {name:"Jordan",},
+    {name:"Blazer",},
+    {name:"Crater",},
+    {name:"Hippie",},
+]
   return (
     <>
-      <div className='bg-black text-white py-[20px] px-[50px]'>
+      <div className="bg-black text-white pt-[20px] pb-[8px] px-[30px] sm:px-[40px] sticky top-0 left-0 z-50">
         <div className="navTop flex flex-col sm:flex-row items-center justify-between">
           <div className="navItem">
-            <h1 className='text-[1.5em] font-semibold'>.Sneakwear</h1>
+            <h1 className="text-[1.5em] font-semibold">.Sneakwear</h1>
           </div>
           <div className="navItem py-2 sm:py-0">
-            <div className='flex items-center bg-gray-500 py-1 sm:py-2 px-2 sm:px-4 rounded-lg'>
+            <div className="flex items-center bg-[#808080] py-1 sm:py-2 px-2 sm:px-4 rounded-lg">
               <input
                 type="text"
                 placeholder="search..."
-                className="searchInput bg-transparent placeholder:text-sm placeholder:text-white outline-none"
-                />
+                className="searchInput bg-transparent cursor-pointer placeholder:text-sm placeholder:text-white outline-none"
+              />
               <i className="ri-search-line"></i>
             </div>
           </div>
-          <div id='progressBarText' className="navItem">
-            <span id='navTop' className="limitedOffer text-[1em] sm:text-[1.3em] cursor-pointer">Limited Offer!</span>
-            <div id='progressBar'></div>
+          <div id="progressBarText" className="navItem">
+            <span
+              id="navTop"
+              className="limitedOffer text-[1em] sm:text-[1.3em] cursor-pointer"
+            >
+              Limited Offer!
+            </span>
+            <div id="progressBar"></div>
           </div>
         </div>
-        <div className="navBottom">
-
+        <div className="navBottom flex items-center justify-center flex-wrap gap-[6px] mt-[20px]">
+          {menuItemsName.map((data) => (
+            <h3 className="menuItem uppercase text-sm sm:text-[1rem] w-[80px] sm:w-[130px] text-center cursor-pointer text-gray-300 ">
+              {data.name}
+            </h3>
+          ))}
         </div>
       </div>
     </>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import 'remixicon/fonts/remixicon.css'
 
 function Home() {
   return (
     <>
+    <div className="slider bg-[url(https://images.unsplash.com/photo-1550684376-efcbd6e3f031?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80)]" 
+    style={{clipPath: `polygon(0 0, 100% 0%, 100% 100%, 0 85%)`, height:`calc(100vh - 130px)`}}
+    >
+      <div className="sliderWrapper flex">
+        <div className="sliderItem relative w-full flex items-center justify-center">
+          <img src="https://cdn.leonardo.ai/users/36eaba97-5f1a-4a31-a92a-b2ea8256702f/generations/e3467ff9-70f0-4027-bbce-6db7a5c7b1ee/variations/Default_create_nike_air_force_shoe_in_black_1_e3467ff9-70f0-4027-bbce-6db7a5c7b1ee_0.png" alt="slide1" className='absolute top-0 object-cover w-[200px]'/>
+          <div className="slideBg absolute w-[300px] h-[300px] rounded-[50%] bg-red-500 -z-10 top-0"></div>
+          <h1 className='sliderTitle absolute text-white top-[10%] right-[10%] text-center text-[2em] font-black'>Air Force <br /> New <br /> Season</h1>
+          <p className="slidePrice absolute text-white top-[10%] left-[10%] text-center text-[5vw]">$119
+          <div className='priceProgressBar'></div>
+          </p>
+          <button className="buyButton absolute text-white top-[33%] right-[10%] text-center text-[30px] font-black border border-gray-500 cursor-pointer bg-black py-1 px-2 ">Buy Now!</button>
+        </div>
+        {/* <div className="sliderItem w-full flex items-center justify-center">
+          <img src="https://cdn.leonardo.ai/users/36eaba97-5f1a-4a31-a92a-b2ea8256702f/generations/150a3d2b-9372-4997-8641-b92e2c8152f3/variations/Default_blue_nike_sneaker_2_150a3d2b-9372-4997-8641-b92e2c8152f3_0.png" alt="slide2" />
+          <div className="slideBg"></div>
+          <h1 className='sliderTitle'>Air Force <br /> New <br /> Season</h1>
+          <h2 className="slidePrice">$119</h2>
+          <button className="buyButton">Buy Now!</button>
+        </div>
+        <div className="sliderItem w-full flex items-center justify-center">
+          <img src="https://cdn.leonardo.ai/users/36eaba97-5f1a-4a31-a92a-b2ea8256702f/generations/479182a9-ff0e-4bf6-814d-0aca04cd0706/variations/Default_green_air_jordan_sneaker_3_479182a9-ff0e-4bf6-814d-0aca04cd0706_0.png" alt="slide3" />
+          <div className="slideBg"></div>
+          <h1 className='sliderTitle'>Air Force <br /> New <br /> Season</h1>
+          <h2 className="slidePrice">$119</h2>
+          <button className="buyButton">Buy Now!</button>
+        </div> */}
+      </div>
+    </div>
     </>
   )
 }
