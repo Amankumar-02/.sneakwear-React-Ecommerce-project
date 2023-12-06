d5dfcca445adbce6d0e0d1415c707d448417d755
Aman <ak193566@gmail.com>
2023-12-06 23:30:54 +0530

    hero div done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,13 +11,13 @@ function Header() {
 ]
   return (
     <>
-      <div className="bg-black text-white pt-[20px] pb-[8px] px-[30px] sm:px-[40px] sticky top-0 left-0 z-50">
+      <div className="bg-black text-white pt-[4px] sm:pt-[20px] pb-[8px] px-[20px] sm:px-[40px] sticky top-0 left-0 z-50 border-b border-gray-300">
         <div className="navTop flex flex-col sm:flex-row items-center justify-between">
           <div className="navItem">
             <h1 className="text-[1.5em] font-semibold">.Sneakwear</h1>
           </div>
-          <div className="navItem py-2 sm:py-0">
-            <div className="flex items-center bg-[#808080] py-1 sm:py-2 px-2 sm:px-4 rounded-lg">
+          <div className="navItem py-1 sm:py-0">
+            <div className="flex items-center bg-[#808080] sm:py-2 px-2 sm:px-4 rounded-lg">
               <input
                 type="text"
                 placeholder="search..."
@@ -29,16 +29,16 @@ function Header() {
           <div id="progressBarText" className="navItem">
             <span
               id="navTop"
-              className="limitedOffer text-[1em] sm:text-[1.3em] cursor-pointer"
+              className="limitedOffer text-[12px] sm:text-[1.3em] cursor-pointer"
             >
               Limited Offer!
             </span>
             <div id="progressBar"></div>
           </div>
         </div>
-        <div className="navBottom flex items-center justify-center flex-wrap gap-[6px] mt-[20px]">
+        <div className="navBottom flex items-center justify-center flex-wrap gap-3 sm:gap-[6px] mt-2 sm:mt-[20px]">
           {menuItemsName.map((data) => (
-            <h3 className="menuItem uppercase text-sm sm:text-[1rem] w-[80px] sm:w-[130px] text-center cursor-pointer text-gray-300 ">
+            <h3 className="menuItem uppercase text-sm sm:text-[1rem] sm:w-[130px] text-center cursor-pointer text-gray-300 hover:text-[1.2rem]">
               {data.name}
             </h3>
           ))}
