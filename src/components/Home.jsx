e1add4bdb365f8840d9fb0c5f5eef1313cb956db
Aman <ak193566@gmail.com>
2023-12-11 12:16:30 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,11 +4,11 @@ import { useSelector } from 'react-redux'
 
 function Home() {
   const slideData = [
-    {img:"https://cdn.leonardo.ai/users/36eaba97-5f1a-4a31-a92a-b2ea8256702f/generations/e3467ff9-70f0-4027-bbce-6db7a5c7b1ee/variations/Default_create_nike_air_force_shoe_in_black_1_e3467ff9-70f0-4027-bbce-6db7a5c7b1ee_0.png", color:"bg-red-500", text:"text-red-500", title:"air force",}, 
-    {img:"https://cdn.leonardo.ai/users/36eaba97-5f1a-4a31-a92a-b2ea8256702f/generations/1cf3721e-8eb6-4a6d-bbea-75d1af8ae239/variations/Default_create_high_quality_image_with_air_jordan_shoe_in_yell_2_1cf3721e-8eb6-4a6d-bbea-75d1af8ae239_0.png", color:"bg-yellow-500", text:"text-yellow-500", title:"air jordan",},
-    {img:"https://cdn.leonardo.ai/users/36eaba97-5f1a-4a31-a92a-b2ea8256702f/generations/5e134d85-5475-41c2-bc7f-b651e3185e2f/variations/Default_nike_air_jordan_blue_sneaker_0_5e134d85-5475-41c2-bc7f-b651e3185e2f_0.png", color:"bg-blue-500", text:"text-blue-500" , title:"blazer",},
-    {img:"https://cdn.leonardo.ai/users/36eaba97-5f1a-4a31-a92a-b2ea8256702f/generations/3810e149-a135-4289-9f34-505af9ad68a1/variations/Default_create_nike_air_jordan_shoe_in_black_2_3810e149-a135-4289-9f34-505af9ad68a1_0.png", color:"bg-gray-500", text:"text-gray-500", title:"crater",},
-    {img:"https://cdn.leonardo.ai/users/36eaba97-5f1a-4a31-a92a-b2ea8256702f/generations/479182a9-ff0e-4bf6-814d-0aca04cd0706/variations/Default_green_air_jordan_sneaker_3_479182a9-ff0e-4bf6-814d-0aca04cd0706_0.png", color:"bg-green-500", text:"text-green-500", title:"hippie",},
+    {img:"https://cdn.leonardo.ai/users/36eaba97-5f1a-4a31-a92a-b2ea8256702f/generations/e3467ff9-70f0-4027-bbce-6db7a5c7b1ee/variations/Default_create_nike_air_force_shoe_in_black_1_e3467ff9-70f0-4027-bbce-6db7a5c7b1ee_0.png", color:"bg-red-500", text:"text-red-500", title:"air force", price:"$199"}, 
+    {img:"https://cdn.leonardo.ai/users/36eaba97-5f1a-4a31-a92a-b2ea8256702f/generations/1cf3721e-8eb6-4a6d-bbea-75d1af8ae239/variations/Default_create_high_quality_image_with_air_jordan_shoe_in_yell_2_1cf3721e-8eb6-4a6d-bbea-75d1af8ae239_0.png", color:"bg-yellow-500", text:"text-yellow-500", title:"air jordan", price:"$299"},
+    {img:"https://cdn.leonardo.ai/users/36eaba97-5f1a-4a31-a92a-b2ea8256702f/generations/5e134d85-5475-41c2-bc7f-b651e3185e2f/variations/Default_nike_air_jordan_blue_sneaker_0_5e134d85-5475-41c2-bc7f-b651e3185e2f_0.png", color:"bg-blue-500", text:"text-blue-500" , title:"blazer", price:"$149"},
+    {img:"https://cdn.leonardo.ai/users/36eaba97-5f1a-4a31-a92a-b2ea8256702f/generations/3810e149-a135-4289-9f34-505af9ad68a1/variations/Default_create_nike_air_jordan_shoe_in_black_2_3810e149-a135-4289-9f34-505af9ad68a1_0.png", color:"bg-gray-500", text:"text-gray-500", title:"crater", price:"$179"},
+    {img:"https://cdn.leonardo.ai/users/36eaba97-5f1a-4a31-a92a-b2ea8256702f/generations/479182a9-ff0e-4bf6-814d-0aca04cd0706/variations/Default_green_air_jordan_sneaker_3_479182a9-ff0e-4bf6-814d-0aca04cd0706_0.png", color:"bg-green-500", text:"text-green-500", title:"hippie", price:"$159"},
   ]
   const featureData = [
     {img:"https://img.icons8.com/?size=200&id=8290&format=png&color=EF4444", img2:"https://img.icons8.com/wired/200/EF4444/free-shipping.png", title:"Free Shipping", desc:"Free Worldwide Shipping On All Orders."},
@@ -19,7 +19,7 @@ function Home() {
   const [productData, setProductData] = useState([
     {
       title: "",
-      price: "",
+      price: "$149",
       desc: "",
       displayImg:"https://cdn.leonardo.ai/users/8d611489-0d5f-4751-a2ec-062c4b782ea8/generations/f6237ddb-744b-4255-8f04-bf20a0ac1bd2/variations/Default_nike_air_jordan_shoes_3_f6237ddb-744b-4255-8f04-bf20a0ac1bd2_0.png",
       item1: {
@@ -33,7 +33,7 @@ function Home() {
     },
     {
       title: "",
-      price: "",
+      price: "$175",
       desc: "",
       displayImg:"https://cdn.leonardo.ai/users/36eaba97-5f1a-4a31-a92a-b2ea8256702f/generations/fb5734ff-020c-4a8d-8407-52da9fea1af7/variations/Default_nike_air_jordan_orange_sneaker_2_fb5734ff-020c-4a8d-8407-52da9fea1af7_0.png",
       item1: {
@@ -47,7 +47,7 @@ function Home() {
     },
     {
       title: "",
-      price: "",
+      price: "$119",
       desc: "",
       displayImg:"https://cdn.leonardo.ai/users/9e51583c-3ad7-46cd-8cb1-d09fc26ccdbf/generations/a1b280f1-8dfd-4201-88fe-07ac1cc8aba7/variations/Default_Nike_Shoes_3_a1b280f1-8dfd-4201-88fe-07ac1cc8aba7_0.png",
       item1: {
@@ -61,7 +61,7 @@ function Home() {
     },
     {
       title: "",
-      price: "",
+      price: "$229",
       desc: "",
       displayImg:"https://cdn.leonardo.ai/users/9e51583c-3ad7-46cd-8cb1-d09fc26ccdbf/generations/b6a4dc15-0862-4f73-bf3b-206662d203aa/variations/Default_air_jordan_2_b6a4dc15-0862-4f73-bf3b-206662d203aa_0.png",
       item1: {
@@ -75,7 +75,7 @@ function Home() {
     },
     {
       title: "",
-      price: "",
+      price: "$249",
       desc: "",
       displayImg:"https://cdn.leonardo.ai/users/8d611489-0d5f-4751-a2ec-062c4b782ea8/generations/54a1cb9a-9528-4a5d-a389-c24494b0fdb9/variations/Default_nike_sneakers_1_54a1cb9a-9528-4a5d-a389-c24494b0fdb9_0.png",
       item1: {
@@ -125,7 +125,7 @@ function Home() {
           <div className={`slideBg absolute w-[220px] sm:w-[500px] h-[220px] sm:h-[500px] rounded-[50%] ${data.color} z-1 top-[10px] sm:top-[-2px] left-[50%] translate-x-[-50%]`}></div>
           <h1 className='sliderTitle absolute text-white top-[12%] sm:top-[30%] left-[7%] sm:left-[76%] sm:w-[250px] sm:text-center sm:text-[40px] leading-[1.2] font-bold uppercase'>{data.title} <br /> New <br /> Season</h1>
           <div className={`slidePrice absolute ${data.text} top-[40%] left-[8%] sm:left-[10%] text-center text-[5vw]`}>
-          <p>$119</p>
+          <p>{data.price}</p>
           <div className={`priceProgressBar bg-gray-300 h-[2px] sm:h-[4px] mt-[-4px] sm:mt-[-14px]`}></div>
           </div>
           <button className="buyButton absolute text-white top-[70%] sm:top-[65%] right-[3%] sm:right-[8%] text-center text-xs sm:text-[28px] font-black border border-gray-500 cursor-pointer bg-black py-1 sm:py-4 px-1 sm:px-4 hover:text-black hover:bg-white rounded-lg">Buy Now!</button>
@@ -144,7 +144,8 @@ function Home() {
       </div>
       ))}
     </div>
-    <div className="products w-full overflow-hidden">
+    <div 
+    className="products w-full overflow-hidden">
       <div className="productWrapper flex w-[500vw]" style={storeData}>
         {productData.map((data, index)=>(
           <div
@@ -152,8 +153,8 @@ function Home() {
           className="product w-full h-[240px] sm:h-[500px]">
             <img src={data.displayImg} alt="product1" className='w-[60%] sm:w-[48%] absolute top-[30px] sm:top-[18px] left-[2%] rotate-[12deg]' />
             <div className="productDetails absolute top-[8%] sm:top-[2%] right-[2%] w-[40%] p-[20px] sm:p-[50px]">
-              <h1 className=" productTitle text-[18px] sm:text-[60px] font-bold text-white sm:tracking-[-2px] leading-[1]">Air Force</h1>
-              <h2 className="productPrice text-white text-[14px] sm:text-[30px] mt-[4px] sm:mt-[10px]">$199</h2>
+              <h1 className=" productTitle text-[18px] sm:text-[60px] font-bold text-white sm:tracking-[-2px] leading-[1] capitalize">{slideData[index].title}</h1>
+              <h2 className="productPrice text-white text-[14px] sm:text-[30px] mt-[4px] sm:mt-[10px]">{data.price}</h2>
               <p className='productDesc1 text-xs sm:text-[16px] text-gray-300'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi illum ad earum necessitatibus quidem. Dolorum tempore earum exercitationem sunt sed natus, cupiditate iusto fugiat quasi inventore, quod vitae alias eum.</p>
               <p className='productDesc2 text-[9px] sm:text-[16px] text-gray-300'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
             </div>
