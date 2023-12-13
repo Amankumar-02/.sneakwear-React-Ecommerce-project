897ea22a23a33e46648f00691bec8199a3537c53
Aman <ak193566@gmail.com>
2023-12-13 18:27:12 +0530

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import 'remixicon/fonts/remixicon.css';
 
 function Footer() {
   const footerData = [
@@ -33,8 +34,26 @@ function Footer() {
           </div>
         ))}
       </div>
-      <div className="footerRight w-[35%] h-full py-[24px] bg-green-400 sm:bg-green-500">
-        sc
+      <div className="footerRight w-[35%] h-full py-[24px] bg-green-400 sm:bg-green-500 flex flex-col justify-between items-center">
+        <div className="footerRightMenu">
+          <h1 className="fMenuTitle text-lg -tracking-tighter">Subscribe to our newsletter</h1>
+          <div className="fMenuMail">
+            <input type="text" placeholder='your@email.com' className='p-[5px] outline-none'/>
+            <button className='fMenuButton ms-1 p-[5px] bg-black text-white hover:border hover:border-black hover:text-black hover:bg-transparent hover:font-semibold'>Join !</button>
+          </div>
+        </div>
+        <div className="footerRightMenu flex my-5">
+          <h1 className="fMenuTitle text-xl">Follow Us</h1>
+          <div className="fIcons ms-[30px]">
+          <i className="ri-facebook-circle-line me-2 text-2xl cursor-pointer hover:text-[28px]"></i>
+          <i className="ri-twitter-x-line me-2 text-2xl cursor-pointer hover:text-[28px]"></i>
+          <i className="ri-instagram-line me-2 text-2xl cursor-pointer hover:text-[28px]"></i>
+          <i className="ri-whatsapp-line text-2xl cursor-pointer hover:text-[28px]"></i>
+          </div>
+        </div>
+        <div className="footerRightMenu">
+          <span className="copyRight"><i class="ri-at-line"></i>AmanKr. All rights reserved, 2024.</span>
+        </div>
       </div>
     </div>
     </>
